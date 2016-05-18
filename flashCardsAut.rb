bada075aed861065faa86e36d37f0b476766be4d
MarianaSoriano <mariana.soriano.licona@gmail.com>
2016-05-18 14:20:38 -0500

    Flash Cards

--- flashCardsAut.rb
+++ flashCardsAut.rb
@@ -1,8 +1,14 @@
 def flashCard(string, word_number)
   @phrase = string
-  words_in_phrase = @phrase.split(" ")#Array
+  words_in_phrase = @phrase.split(" ") #Array
   @chosen_word = words_in_phrase[word_number - 1]
-  # front_card =
-  p "#{@phrase.gsub(@chosen_word, '[…]')} , #{@phrase.gsub(@chosen_word, @chosen_word.upcase)}"
+  if @phrase[-1] == '?'
+    @phrase.gsub!(' ?','?')
+  end
+  front_card = @phrase.gsub(@chosen_word, '[…]')
+  back_card = @phrase.gsub(@chosen_word, @chosen_word.upcase)
+  p "#{front_card} , #{back_card}"
 end
 flashCard("Who was that man you were talking to ?", 8)
+flashCard("Can you explain this word to me ?",6)
+flashCard("Let me describe to you what I saw.", 4)
